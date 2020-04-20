022ade1bc45f666a4849215e94e757cabfbf9d30
蓝莓铁匠 <yeah2109@yeah.net>
2020-04-20 22:48:19 +0800

    mac 3-3 gulp

---
+++ 3-3/dist/js/index.js
@@ -0,0 +1,1 @@
+console.log("hello imooc44"),console.log("hello imooc33"),console.log("hello imooc111"),console.log("hello imooc22"),console.log("hello imooc"),console.log("hello imooc");
---
+++ 3-3/js/index.js
@@ -0,0 +1,6 @@
+console.log('hello imooc44');
+console.log('hello imooc33');
+console.log('hello imooc111');
+console.log('hello imooc22');
+console.log('hello imooc');
+console.log('hello imooc');
