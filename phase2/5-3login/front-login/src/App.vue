e64d2359cbacb7b6cb4f2f852d85eff0920a7d09
蓝莓铁匠 <yeah2109@yeah.net>
2020-07-28 16:48:44 +0800

    new document

---
+++ phase2/5-3login/front-login/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action>
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input
+              type="password"
+              name="password"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-block">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <div class="layui-input-block">
+            <button class="layui-btn" lay-submit lay-filter="formDemo">登录</button>
+            <button type="reset" class="layui-btn layui-btn-primary">忘记密码</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+#app {
+  background: #f2f2f2;
+}
+.layui-container {
+  background: #fff;
+}
+input {
+  width: 190px;
+}
+</style>
